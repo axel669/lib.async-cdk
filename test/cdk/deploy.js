ad74f1efec12a6902288363e05f761a276c9541e
axel669 <haryuu.hanekata@gmail.com>
2024-09-06 00:20:00 -0700

    [change] make the stack contain everything it needs

--- test/cdk/deploy.js
+++ test/cdk/deploy.js
@@ -2,7 +2,6 @@ import cdk from "aws-cdk-lib"
 
 import { TestStack } from "./test-stack.js"
 
-import { initStack } from "../../lib/main.js"
-
 const app = new cdk.App()
-await initStack(TestStack, app, "nonprod", {})
+// await initStack(TestStack, app, "nonprod", {})
+await TestStack.create(app, "nonprod", {})
