5932030c095f815c698d991f9d4cf3807598eb6f
axel669 <haryuu.hanekata@gmail.com>
2024-09-06 01:04:40 -0700

    [change] slightly modify the test deployment

--- test/cdk/deploy.js
+++ test/cdk/deploy.js
@@ -3,5 +3,5 @@ import cdk from "aws-cdk-lib"
 import { TestStack } from "./test-stack.js"
 
 const app = new cdk.App()
-// await initStack(TestStack, app, "nonprod", {})
-await TestStack.create(app, "nonprod", {})
+await TestStack.create(app, "dev", {})
+await TestStack.create(app, "prod", {})
